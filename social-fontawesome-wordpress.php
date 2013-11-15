fc39023f032245e81e3c61007a607bb0b19b6c92
Michael Bianco <iloveitaly@gmail.com>
2013-11-15 15:51:45 -0500

    Initial commit

---
+++ social-fontawesome-wordpress.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+Plugin Name: social-fontawesome-wordpress
+Description: Simple Social Icons Using FontAwesome for Wordpress
+
+Author: Michael Bianco (@iloveitaly; http://cliffsidedev.com/)
+Author URI: https://github.com/iloveitaly/social-fontawesome-wordpress
+Plugin URI: https://github.com/iloveitaly/social-fontawesome-wordpress
+
+Description: Simple essential sharing icons with fontawesome. Clean code, easy to customize.
+
+Version: 1.0
+License: MIT
+*/
+
+if(!defined( 'ABSPATH' )) exit;
+
+define('SOCIAL_FA_WIDGET_PATH', plugin_dir_path( __FILE__ ));
+define('SOCIAL_FA_PLUGIN_URL', plugin_dir_url(  __FILE__  ) );
+
+add_filter('the_content', 'social_fa_add_icons');
+function social_fa_add_icons($content) {
+	$url = urlencode(get_permalink());
+	$title = urlencode(get_the_title());
+
+	ob_start();
+
+	// allow the user to customize the template without forking the plugin
+	if($overridden_template = locate_template('templates/social-fa-sharing.php')) {
+		include $overridden_template;
+	} else {
+		include SOCIAL_FA_WIDGET_PATH . 'templates/sharing.php';
+	}
+
+	$output = ob_get_contents();
+	ob_end_clean();
+
+	$content .= $output;
+
+	return $content;
+}
+
+// add styles
+add_action('wp_enqueue_scripts', 'social_fa_assets');
+function social_fa_assets() {
+	wp_enqueue_style('social-fa-style', SOCIAL_FA_PLUGIN_URL . 'style.css');
+}
---
+++ templates/sharing.php
@@ -0,0 +1,6 @@
+<aside id="social-fontawesome">
+	<a href="http://www.facebook.com/sharer.php?u=<?php echo $url; ?>&t=<?php echo $title; ?>" target="_blank" title="Share on Facebook"><i class="icon-facebook"></i></a>
+	<a href="http://twitter.com/share?url=<?php echo $url; ?>&text=<?php echo $title; ?>" target="_blank" title="Share on Twitter"><i class="icon-twitter"></i></a>
+	<a href="https://plus.google.com/share?url=<?php echo $url; ?>" target="_blank" title="Share on Google Plus"><i class="icon-google-plus"></i></a>
+	<a href="http://pinterest.com/pin/create/button/?url=<?php echo $url; ?>&media=&description=<?php echo $title; ?>" target="_blank" title="Share on Pinterest"><i class="icon-pinterest"></i></a>
+</aside>
