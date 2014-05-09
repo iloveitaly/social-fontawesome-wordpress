63280d1833595f3249a3d2190afd846e2eecdac1
Michael Bianco <iloveitaly@gmail.com>
2014-05-09 11:40:15 -0400

    Adding email share button

--- templates/sharing.php
+++ templates/sharing.php
@@ -3,4 +3,5 @@
 	<a href="http://twitter.com/share?url=<?php echo $url; ?>&text=<?php echo $title; ?>" target="_blank" title="Share on Twitter"><i class="fa fa-twitter"></i></a>
 	<a href="https://plus.google.com/share?url=<?php echo $url; ?>" target="_blank" title="Share on Google Plus"><i class="fa fa-google-plus"></i></a>
 	<a href="http://pinterest.com/pin/create/button/?url=<?php echo $url; ?>&media=&description=<?php echo $title; ?>" target="_blank" title="Share on Pinterest"><i class="fa fa-pinterest"></i></a>
+	<a href="mailto:?subject=<?php echo urldecode($title); ?>&amp;body=Check out this article on <?php echo get_bloginfo(); ?>%0D%0A%0D%0A<?php echo $url; ?>" target="_blank" title="Share using Email"><i class="fa fa-envelope"></i></a>
 </aside>
